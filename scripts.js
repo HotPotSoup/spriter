31d250455198ad90d57816c16bcc57dc37a63c5a
HotPotSoup <gaosteven@rocketmail.com>
2018-10-10 18:07:35 -0400

    changed sprites and removed magic numbers

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ var srcX = 0;
 var srcY = 0;
 var row = 0;
 var fps = 12;
-var sheetRows = 3;
+var sheetRows = 4;
+var spriteNum = 6;
 
 function setup() {
     setInterval(drawAnimation, 1000 / fps);
@@ -16,8 +17,8 @@ function drawAnimation() {
         img.src = 'sprites/' + i + '.png';
         canvas[i].width = img.width;
         canvas[i].height = img.height;
-        spriteWidth = canvas[i].width / 4;
-        spriteHeight = canvas[i].height / 4;
+        spriteWidth = canvas[i].width / spriteNum;
+        spriteHeight = canvas[i].height / sheetRows;
         ctx.clearRect(0, 0, canvas[i].width, canvas[i].height);
         ctx.drawImage(img, srcX, srcY, spriteWidth, spriteHeight,
             0, 0, canvas[i].width, canvas[i].height);
@@ -25,7 +26,7 @@ function drawAnimation() {
     srcX = srcX + spriteWidth;
     if (srcX > img.width - spriteWidth) {
         srcX = 0;
-        if (row < sheetRows) {
+        if (row + 1 < sheetRows) {
             row++;
             } else {
                 row = 0;
